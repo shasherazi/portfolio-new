9164ff03ef4b944f0b117b9355f1ee76631b54d1
shasherazi <hassanrandomz@gmail.com>
2023-05-03 00:10:56 +0500

    fix linter error

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>shasherazi's website</title>
+        <title>shasherazi&apos;s website</title>
         <meta name="description" content="a personal website for shasherazi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
