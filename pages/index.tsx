aa53200f9d2b1ae2109680dc773b6dbb22c0da1d
shasherazi <hassanrandomz@gmail.com>
2023-05-22 09:30:19 +0500

    Finished home page minimally

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,22 +12,65 @@ export default function Home() {
       </Head>
       <Nav />
       <main className={styles.main}>
-        <h1 className={styles.title}>shasherazi</h1>
+        <h1 className={styles.title}>
+          <a href="https://github.com/shasherazi" target="_blank">
+            shasherazi
+          </a>
+        </h1>
         <p className={styles.description}>
-          this is my personal website. it&apos;s a work in progress. Lorem ipsum
-          dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore
-          culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.
-          Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud
-          ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est
-          proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea
-          nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis
-          laboris cupidatat officia voluptate. Culpa proident adipisicing id
-          nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
-          reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate
-          laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa
-          duis. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
-          sint cillum sint consectetur cupidatat.
+          im shasherazi, a 20 year old{" "}
+          <a
+            className="underline"
+            href="https://en.wikipedia.org/wiki/Pakistan"
+            target="_blank"
+          >
+            pakistani{" "}
+          </a>{" "}
+          coder with too many interests, and almost all of them include
+          computers one way or another. i like linux, python, rust, chess,
+          reading, and learning new things.
         </p>
+
+        <h2 className={`${styles.title} ${styles.projects}`}>projects</h2>
+        <div className={styles.project}>
+          <h3 className={styles.subtitle}>
+            <a href="https://github.com/shasherazi/dotfiles" target="_blank">
+              dotfiles
+            </a>
+          </h3>
+          <p className={styles.description}>
+            my dotfiles for my arch linux setup
+          </p>
+        </div>
+
+        <div className={styles.project}>
+          <h3 className={styles.subtitle}>
+            <a href="https://github.com/shasherazi/dmenu-emoji" target="_blank">
+              dmenu-emoji
+            </a>
+          </h3>
+          <p className={styles.description}>
+            A script to display and copy emojis for launchers like dmenu and
+            rofi
+          </p>
+        </div>
+
+        <h2 className={`${styles.title} ${styles.contact}`}>contact</h2>
+        <ul className={styles.contact}>
+          <li className={styles.contactItem}>shasherazi#1109 on discord</li>
+          <li className={styles.contactItem}>
+            <a href="https://twitter.com/shasherazi" target="_blank">
+              shasherazi
+            </a>{" "}
+            on twitter
+          </li>
+          <li className={styles.contactItem}>
+            <a href="https://gituhb.com/shasherazi" target="_blank">
+              shasherazi
+            </a>{" "}
+            on github
+          </li>
+        </ul>
       </main>
     </>
   );
