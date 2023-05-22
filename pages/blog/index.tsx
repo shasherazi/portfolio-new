cc5d27fb347a616dd78f0dfa791b5d0f7f5458f2
shasherazi <hassanrandomz@gmail.com>
2023-05-22 09:49:57 +0500

    Added blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/Blog.module.css";
+
+export default function Blog() {
+  return (
+    <>
+      <main className={styles.main}>
+        <h1 className={styles.title}>this page is under construction 🚧</h1>
+      </main>
+    </>
+  );
+}
