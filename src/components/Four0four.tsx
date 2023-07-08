6e594284753fea15a9cc29eef63b0a12a5b0cfad
shasherazi <hassanrandomz@gmail.com>
2023-07-08 22:40:32 +0500

    zalgo text

--- src/components/Four0four.tsx
+++ src/components/Four0four.tsx
@@ -16,7 +16,7 @@ function Four0four() {
       <h1 className="absolute text-center font-playfair text-9xl font-bold text-white">
         404
       </h1>
-      <p className="font-playfair text-xl text-white">page not found</p>
+      <p className="font-playfair text-xl text-white">p̷̻̘̊a̶̮̥̬͂g̷̟̣̺̀̎ě̷̥͎͔̊̍ ̷̨͖̒̊ṇ̴̝̫̔̑̚ǫ̶̓̎͘t̴̯̱̋ ̷̬̠͕̔f̸̙͐̿̌ō̷̭͉͝͠u̸͚̅̒́n̸̨̩̖͊d̷̹̲̑</p>
     </div>
   );
 }
